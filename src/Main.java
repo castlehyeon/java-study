f130bb7b450148d8e939fc3b88d2ff7197283ae5
devsibong <seohae7785@gmail.com>
2023-03-24 14:56:07 +0900

    modify : main.java

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,24 @@ import kr.or.kosa.Beverage2;
 import kr.or.kosa.Payment;
 import kr.or.kosa.VendingMachine2;
 
-<<<<<<< HEAD
 class Util {
-
-=======
-class Util{
-	//음료구매 함수
-	public void buy(int inputMoney,Beverage2 beverage){
+	// 음료구매 함수
+	public void buy(int inputMoney, Beverage2 beverage) {
 		if (inputMoney >= beverage.getPrice()) {
-			System.out.printf("%s 나왔습니다~\n\n",beverage.getName());
+			System.out.printf("%s 나왔습니다~\n\n", beverage.getName());
 			beverage.minusCount();
 			returnChange(inputMoney - beverage.getPrice());
 		} else {
-			returnChange(inputMoney);//돈 반환
+			returnChange(inputMoney);// 돈 반환
 			System.out.println("돈이 부족합니다. 나중에 다시 방문해주세요!");
 		}
 	}
-	
-	//잔돈반환 함수
+
+	// 잔돈반환 함수
 	public void returnChange(int money) {
 		Payment pm = new Payment();
 		pm.returnChange(money);
 	}
->>>>>>> b995432d4a6d7e05f3244a7b73432bbe80a0d28b
 }
 
 public class Main {
@@ -55,56 +50,38 @@ public class Main {
 			Back: switch (control) {
 			case 1:// 음료규매
 				do {
-					System.out.println("금액을 입력해주세요.");
+					System.out.printf("금액을 입력해주세요.");
 					int inputMoney = scanner.nextInt();
 
 					System.out.println("1. 아메리카노[1000원] 2. 라떼[1500원] 3. 망고쥬스[2000원] 4. 취소");
 					System.out.print("메뉴를 선택해주세요. : ");
-
+					Payment pm = new Payment();
 					control = scanner.nextInt();
 
 					switch (control) {
 					case 1: // 아메리카노
-<<<<<<< HEAD
-						if (inputMoney >= ame.getPrice()) {
-							pm.returnChange(inputMoney - ame.getPrice());
-							ame.minusCount();
-							System.out.println("아메리카노 나왔습니다~");
-						} else {
-							pm.returnChange(inputMoney);// 돈 반환
-							System.out.println("다음에 오세요");
-						}
-=======
-						util.buy(inputMoney,ame);
->>>>>>> b995432d4a6d7e05f3244a7b73432bbe80a0d28b
+						util.buy(inputMoney, ame);
 						break Back;
 					case 2: // 라떼
-						util.buy(inputMoney,latte);
+						util.buy(inputMoney, latte);
 						break Back;
 					case 3: // 망고쥬스
-						util.buy(inputMoney,mango);
+						util.buy(inputMoney, mango);
 						break Back;
-<<<<<<< HEAD
 					case 4: // 메인메뉴로 돌아가기
-						pm.returnChange(inputMoney);// 돈 반환
-=======
-					case 4 : //메인메뉴로 돌아가기
-						util.returnChange(inputMoney);//돈 반환
->>>>>>> b995432d4a6d7e05f3244a7b73432bbe80a0d28b
+						util.returnChange(inputMoney);// 돈 반환
 						break Back;
 					}
 
 				} while (exit == false);
 
-<<<<<<< HEAD
 			case 2:
 				do {
 					System.out.println("재고를 채울 음료를 선택하십시오");
 					System.out.println("1. 아메리카노 2. 라떼 3. 망고쥬스 4. 취소");
 					int num = scanner.nextInt();
 					boolean exit2 = false;
-					Stock:
-					switch (num) {
+					Stock: switch (num) {
 					case 1:
 						System.out.printf("아메리카노의 재고는 %d개 입니다.\n", ame.getReserves());
 						System.out.println("재고를 채우시겠습니까? 1.채우기  2. 채우지 않기");
@@ -155,10 +132,6 @@ public class Main {
 						break Back;
 					}
 				} while (exit == false);
-
-=======
-			case 2:// 재고충전
->>>>>>> b995432d4a6d7e05f3244a7b73432bbe80a0d28b
 			case 3:
 				System.out.print("종료합니다.");
 				exit = true;
@@ -168,5 +141,4 @@ public class Main {
 
 	}
 
-	
 }
